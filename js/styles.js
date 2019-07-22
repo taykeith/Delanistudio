df5c88a9c3c3bd40669f035615bdc185e5113769
taykeith <keith.mbugi@gmail.com>
2019-07-22 11:39:29 +0300

    styeld what we do section

--- js/styles.js
+++ js/styles.js
@@ -1,22 +1,23 @@
-(document).ready(function() {
-            $(".block4hidden, .block4showing").click(function() {
-                $(".block4showing").toggle();
-                $(".block4hidden").toggle();
-                $(".block4showing").slideToggle('fast');
-                $(".block4hidden").slideToggle('fast');
-            });
-            $(".block4bhidden, .block4bshowing").click(function() {
-                $(".block4bshowing").toggle();
-                $(".block4bhidden").toggle();
-                $(".block4bshowing").slideToggle('fast');
-                $(".block4bhidden").slideToggle('fast');
-            });
-            $(".block4chidden, .block4cshowing").click(function() {
-                $(".block4cshowing").toggle();
-                $(".block4chidden").toggle();
-                $(".block4cshowing").slideToggle('fast');
-                $(".block4chidden").slideToggle('fast');
-            });
-            $("#work1").hover(function() {
-                $(this).css("background-color", "grey");
-              }
+$(function() {
+    $("#development").click(function() {
+      $("#development-p").toggle();
+      $("#development-img").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $("#design").click(function() {
+      $("#design-p").toggle();
+      $("#design-img").toggle();
+    });
+  });
+  $(document).ready(function() {
+      $("#product-management").click(function() {
+        $("#product-management-p").toggle();
+        $("#product-management-img").toggle();
+      });
+    });
+
+    $("#work1").hover(function() {
+        $(this).css("background-color", "grey");
+      
+    });
