a1476edec9f068574bc0e11791e54f2f4a653670
taykeith <keith.mbugi@gmail.com>
2019-07-22 12:33:17 +0300

    styeld what we do section

--- js/styles.js
+++ js/styles.js
@@ -1,23 +1,35 @@
-$(function() {
-    $("#development").click(function() {
-      $("#development-p").toggle();
-      $("#development-img").toggle();
+$(function(){
+    $('block4showing').hide();
+
+    $(".block4hidden").click(function() {
+        $(".block4showing").show();
+        $(".block4hidden").hide(500);
     });
-  });
-  $(document).ready(function() {
-    $("#design").click(function() {
-      $("#design-p").toggle();
-      $("#design-img").toggle();
+    $(".block4bhidden").click(function() {
+        $(".block4bshowing").show();
+        $(".block4bhidden").hide(500);
     });
-  });
-  $(document).ready(function() {
-      $("#product-management").click(function() {
-        $("#product-management-p").toggle();
-        $("#product-management-img").toggle();
-      });
+    $(".block4chidden").click(function() {
+        $(".block4cshowing").show();
+        $(".block4chidden").hide(500);
+    });
+
+
+    $(".block4showing").click(function() {
+        $(".block4hidden").show();
+        $(".block4showing").hide(500);
+    });
+    $(".block4bshowing").click(function() {
+        $(".block4bhidden").show();
+        $(".block4bshowing").hide(500);
+    });
+    $(".block4cshowing").click(function() {
+        $(".block4chidden").show();
+        $(".block4cshowing").hide(500);
     });
 
     $("#work1").hover(function() {
         $(this).css("background-color", "grey");
       
     });
+});
